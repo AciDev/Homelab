91dfca3a3d06e89e76e0a4556b1e88bc7c18e20f
AciDev <37003392+AciDev@users.noreply.github.com>
2024-11-06 21:00:49 +0000

    Adding some monitoring

---
+++ Notifications/init.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then 
+    echo "Please run this script as root or using sudo!"
+    exit 1
+fi
+
+unit="$(basename $PWD)"
+folder_loc="./../Docker-data/$unit"
+changed=false
+
+directory () {
+    local directory_path="$folder_loc/$1"
+    if [ ! -d "$directory_path" ]; then
+        echo "$1 does not exist."
+        echo "Creating $1."
+        mkdir -p $directory_path
+        changed=true
+    fi
+}
+
+own () {
+    directory "$1"
+    local directory_path="$folder_loc/$1"
+    local user="$(stat -c %u "$directory_path")"
+    local group="$(stat -c %g "$directory_path")"
+    if [ "$user" -ne 1000 ] || [ "$group" -ne 1000 ]; then
+        echo "Chowning 1000:1000 $1"
+        chown 1000:1000 "$directory_path"
+        changed=true
+    fi
+}
+
+# Code goes here
+
+echo "Setting up Ntfy folders"
+directory "Ntfy/data"
+directory "Ntfy/cache"
+chown 1000:1000 "$folder_loc/Ntfy/data"
+chown 1000:1000 "$folder_loc/Ntfy/cache"
+
+echo "Complete"
+echo "$changed"
+exit 0
