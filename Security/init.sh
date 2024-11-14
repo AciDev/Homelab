2dbdeb566d252d18b2f6400cf27537f9283dfb30
AciDev <37003392+AciDev@users.noreply.github.com>
2024-11-14 00:17:05 +0000

    General: Added Security and AI. Adjustments to Media

---
+++ Security/init.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then 
+    echo "Please run this script as root or using sudo!"
+    exit 1
+fi
+
+unit="$(basename $PWD)"
+folder_loc="./../Docker-data/$unit"
+changed=false
+
+directory () {
+    local directory_path="$folder_loc/$1"
+    if [ ! -d "$directory_path" ]; then
+        echo "$1 does not exist."
+        echo "Creating $1."
+        mkdir -p $directory_path
+        changed=true
+    fi
+}
+
+own () {
+    directory "$1"
+    local directory_path="$folder_loc/$1"
+    local user="$(stat -c %u "$directory_path")"
+    local group="$(stat -c %g "$directory_path")"
+    if [ "$user" -ne 1000 ] || [ "$group" -ne 1000 ]; then
+        echo "Chowning 1000:1000 $1"
+        chown 1000:1000 "$directory_path"
+        changed=true
+    fi
+}
+
+# Code goes here
+
+echo "Setting up Vaultwarden folders"
+own "Vaultwarden/data"
+
+echo "Complete"
+echo "$changed"
+exit 0
